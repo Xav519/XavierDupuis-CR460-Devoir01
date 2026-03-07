8676eba7847eca0de76222658fb734ff382f78fb
Xav519 <dupuis_x@yahoo.com>
2026-03-06 21:40:16 -0500

    Étape 10: Docker container

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "pip-devoir-XavierDupuis"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 # Étape 10: Création du groupe de sécurité réseau (NSG) avec une règle pour autoriser le trafic HTTP
