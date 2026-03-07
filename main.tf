1a951b50cca2cd9213c523c185d402b74dc2cfe2
Xav519 <dupuis_x@yahoo.com>
2026-03-06 19:39:09 -0500

    Ajout du VNet et Subnet

--- main.tf
+++ main.tf
@@ -8,3 +8,19 @@ resource "azurerm_resource_group" "rg" {
   name     = "RG-Devoir-Cloud-XavierDupuis"
   location = "East US"
 }
+
+# Étape 8: Création du Réseau Virtuel (VNet)
+resource "azurerm_virtual_network" "vnet" {
+  name                = "VNet-Devoir-XavierDupuis"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Étape 8: Création du Sous-réseau (Subnet)
+resource "azurerm_subnet" "subnet" {
+  name                 = "Subnet-Interne-XavierDupuis"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
