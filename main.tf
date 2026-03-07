3a2dff977e276080f2917c676869730dbee6e13c
Xav519 <dupuis_x@yahoo.com>
2026-03-06 20:03:18 -0500

    Étape 9: Déploiement VM

--- main.tf
+++ main.tf
@@ -24,3 +24,45 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+# Étape 9: 1. Créer l'interface réseau (NIC)
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-devoir-XavierDupuis"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+# Étape 9: 2. Créer la Machine Virtuelle Linux (Ubuntu)
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "VM-devoir-XavierDupuis"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+ # Note: En production, je dois utiliser une méthode plus sécurisée pour les mots de passe, ssh keys, ou Azure Key Vault. Ici, c'est juste pour les besoins du devoir.
+  admin_password                  = "Password"
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
